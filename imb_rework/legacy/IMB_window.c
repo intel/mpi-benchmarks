fae318f7d950ebc8650f48f886823cbb4ec38954
vladimir.vinnitski <vvinnits@nnlmpihsw02.inn.intel.com>
2018-06-14 10:57:31 +0300

    Code cleanup(whitespace).

--- imb_rework/legacy/IMB_window.c
+++ imb_rework/legacy/IMB_window.c
@@ -80,78 +80,67 @@ For more documentation than found here, see
 
 
 void IMB_window(struct comm_info* c_info, int size, struct iter_schedule* ITERATIONS,
-                MODES RUN_MODE, double* time)
+                MODES RUN_MODE, double* time) {
 /*
 
-                      
-                      MPI-2 benchmark kernel
-                      MPI_Win_create + MPI_Win_fence + MPI_Win_free
-                      
+                          MPI-2 benchmark kernel
+                          MPI_Win_create + MPI_Win_fence + MPI_Win_free
 
+Input variables:
 
-Input variables: 
+-c_info                   (type struct comm_info*)
+                          Collection of all base data for MPI;
+                          see [1] for more information
 
--c_info               (type struct comm_info*)                      
-                      Collection of all base data for MPI;
-                      see [1] for more information
-                      
+-size                     (type int)
+                          Basic message size in bytes
 
--size                 (type int)                      
-                      Basic message size in bytes
+-ITERATIONS               (type struct iter_schedule)
+                          Repetition scheduling
 
--ITERATIONS           (type struct iter_schedule)                      
-                      Repetition scheduling
+-RUN_MODE                 (type MODES)
+                          Mode (aggregate/non aggregate; blocking/nonblocking);
+                          see "IMB_benchmark.h" for definition
 
--RUN_MODE             (type MODES)                      
-                      Mode (aggregate/non aggregate; blocking/nonblocking);
-                      see "IMB_benchmark.h" for definition
+Output variables:
 
-
-Output variables: 
-
--time                 (type double*)                      
-                      Timing result per sample
-
+-time                     (type double*)
+                          Timing result per sample
 
 */
-{
-  double t1, t2;
-  int    i, dum;
+    double t1, t2;
+    int    i, dum;
 
-  ierr = 0;
+    ierr = 0;
 
-  if(c_info->rank!=-1)
-    {
-      for(i=0; i<N_BARR; i++) MPI_Barrier(c_info->communicator);
+    if (c_info->rank != -1) {
+        for (i = 0; i < N_BARR; i++)
+            MPI_Barrier(c_info->communicator);
 
-      t1 = MPI_Wtime();
-      for(i=0;i< ITERATIONS->n_sample;i++)
-	{
-          ierr = MPI_Win_create(c_info->r_buffer,size,1,MPI_INFO_NULL,
-                                c_info->communicator, &c_info->WIN);
-          MPI_ERRHAND(ierr);
-          ierr = MPI_Win_fence(0, c_info->WIN);
-          MPI_ERRHAND(ierr);
-/* July 2002 fix V2.2.1, empty window case */
-          if(size>0)
-          {
-          ierr = MPI_Put(c_info->s_buffer, 1, c_info->s_data_type,
-                         c_info->rank, 0, 1, c_info->r_data_type, c_info->WIN);
-          MPI_ERRHAND(ierr);
-          }
+        t1 = MPI_Wtime();
+        for (i = 0; i < ITERATIONS->n_sample; i++) {
+            ierr = MPI_Win_create(c_info->r_buffer, size, 1, MPI_INFO_NULL,
+                                  c_info->communicator, &c_info->WIN);
+            MPI_ERRHAND(ierr);
+            ierr = MPI_Win_fence(0, c_info->WIN);
+            MPI_ERRHAND(ierr);
+            /* July 2002 fix V2.2.1, empty window case */
+            if (size > 0) {
+                ierr = MPI_Put(c_info->s_buffer, 1, c_info->s_data_type,
+                               c_info->rank, 0, 1, c_info->r_data_type, c_info->WIN);
+                MPI_ERRHAND(ierr);
+            }
 
-          ierr = MPI_Win_fence(0, c_info->WIN);
-          MPI_ERRHAND(ierr);
-	  
-          ierr = MPI_Win_free(&c_info->WIN);
-          MPI_ERRHAND(ierr);
-	}
-      t2 = MPI_Wtime();
-      *time=(t2 - t1)/(ITERATIONS->n_sample);
+            ierr = MPI_Win_fence(0, c_info->WIN);
+            MPI_ERRHAND(ierr);
+
+            ierr = MPI_Win_free(&c_info->WIN);
+            MPI_ERRHAND(ierr);
+        }
+        t2 = MPI_Wtime();
+        *time = (t2 - t1) / (ITERATIONS->n_sample);
     }
-  else
-    { 
-      *time = 0.; 
-    }
+    else
+        *time = 0.;
 }
 
