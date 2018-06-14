e2f93a570fa42fac16e0d4d3b60f15ba4883ac54
vladimir.vinnitski <vvinnits@nnlmpihsw02.inn.intel.com>
2018-06-14 10:50:42 +0300

    Code cleanup(whitespace). IMB_g_info.c

--- imb_rework/legacy/IMB_g_info.c
+++ imb_rework/legacy/IMB_g_info.c
@@ -84,41 +84,37 @@ extern FILE* unit;
 
 
 
-void IMB_general_info()
+void IMB_general_info() {
 /*
 
-                      
-                      Prints to stdout some basic information 
+                      Prints to stdout some basic information
                       (Version, time, system (see 'IMB_make_sys_info'))
-                      
-
 
 */
-{
-  /*void IMB_make_sys_info();*/
-  time_t T;
-  
-  time(&T);
-  fprintf(unit,"#------------------------------------------------------------\n");
+    /*void IMB_make_sys_info();*/
+    time_t T;
 
-#ifdef MPI1                 
-  fprintf(unit,"#    Intel(R) MPI Benchmarks %s, MPI-1 part    \n",VERSION);
+    time(&T);
+    fprintf(unit, "#------------------------------------------------------------\n");
+
+#ifdef MPI1
+    fprintf(unit, "#    Intel(R) MPI Benchmarks %s, MPI-1 part    \n", VERSION);
 #elif defined EXT
-  fprintf(unit,"#    Intel(R) MPI Benchmarks %s, MPI-2 part    \n",VERSION);
+    fprintf(unit, "#    Intel(R) MPI Benchmarks %s, MPI-2 part    \n", VERSION);
 #elif defined MPIIO
-  fprintf(unit,"#    Intel(R) MPI Benchmarks %s, MPI-IO part   \n",VERSION);
+    fprintf(unit, "#    Intel(R) MPI Benchmarks %s, MPI-IO part   \n", VERSION);
 #elif defined NBC
-  fprintf(unit,"#    Intel(R) MPI Benchmarks %s, MPI-NBC part  \n",VERSION);
+    fprintf(unit, "#    Intel(R) MPI Benchmarks %s, MPI-NBC part  \n", VERSION);
 #elif defined RMA
-  fprintf(unit,"#    Intel(R) MPI Benchmarks %s, MPI-RMA part  \n",VERSION);
+    fprintf(unit, "#    Intel(R) MPI Benchmarks %s, MPI-RMA part  \n", VERSION);
 #endif
 
 
-  fprintf(unit,"#------------------------------------------------------------\n");
-  fprintf(unit,"# Date                  : %s",asctime(localtime(&T)));
+    fprintf(unit, "#------------------------------------------------------------\n");
+    fprintf(unit, "# Date                  : %s", asctime(localtime(&T)));
 
-  IMB_make_sys_info();
-  fprintf(unit,"\n");
+    IMB_make_sys_info();
+    fprintf(unit, "\n");
 }
 
 /* IMB 3.1 << */
@@ -132,173 +128,152 @@ void IMB_general_info()
 /* >> IMB 3.1  */
 
 
-void IMB_make_sys_info()
+void IMB_make_sys_info() {
 /*
 
-                      
                       Prints to stdout some basic information about the system
                       (outcome of the 'uname' command)
-                      
-
 
 */
-{
-  int dont_care, mpi_subversion, mpi_version;
-/* IMB 3.1 << */
+    int dont_care, mpi_subversion, mpi_version;
+    /* IMB 3.1 << */
 #ifndef WIN_IMB
-  struct utsname info;
-  uname( &info );
-  dont_care = MPI_Get_version(&mpi_version,&mpi_subversion);
-  
-  fprintf(unit,"# Machine               : %s\n",info.machine);
-  fprintf(unit,"# System                : %s\n",info.sysname);
-  fprintf(unit,"# Release               : %s\n",info.release);
-  fprintf(unit,"# Version               : %s\n",info.version);
+    struct utsname info;
+    uname(&info);
+    dont_care = MPI_Get_version(&mpi_version, &mpi_subversion);
+
+    fprintf(unit, "# Machine               : %s\n", info.machine);
+    fprintf(unit, "# System                : %s\n", info.sysname);
+    fprintf(unit, "# Release               : %s\n", info.release);
+    fprintf(unit, "# Version               : %s\n", info.version);
 #else
-/* include WIN case */
-  OSVERSIONINFOEX info;
-  TCHAR infoBuf[INFO_BUFFER_SIZE];
-  DWORD bufCharCount = INFO_BUFFER_SIZE;
-  char *substr_ptr;
+    /* include WIN case */
+    OSVERSIONINFOEX info;
+    TCHAR infoBuf[INFO_BUFFER_SIZE];
+    DWORD bufCharCount = INFO_BUFFER_SIZE;
+    char *substr_ptr;
 
-  dont_care = MPI_Get_version(&mpi_version,&mpi_subversion);
-  
-  info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
-  GetVersionEx((OSVERSIONINFO *) &info);
+    dont_care = MPI_Get_version(&mpi_version, &mpi_subversion);
 
-  bufCharCount = ExpandEnvironmentStrings("%PROCESSOR_IDENTIFIER%",infoBuf,INFO_BUFFER_SIZE);
+    info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+    GetVersionEx((OSVERSIONINFO *)&info);
 
-/* Replace  "Intel64" by "Intel(R) 64" */
-  substr_ptr = strstr(infoBuf, "Intel64");
-  if( substr_ptr != NULL )
-      fprintf(unit,"# Machine               : Intel(R) 64%s\n", substr_ptr+strlen("Intel64"));
-  else
-  {
-      /* Replace  "EM64T" by "Intel(R) 64" */
-      substr_ptr = strstr(infoBuf, "EM64T");
-      if( substr_ptr != NULL )
-	    fprintf(unit,"# Machine               : Intel(R) 64%s\n", substr_ptr+strlen("EM64T"));
-      else
-	    fprintf(unit,"# Machine               : %s\n",infoBuf);
-  }      
+    bufCharCount = ExpandEnvironmentStrings("%PROCESSOR_IDENTIFIER%", infoBuf, INFO_BUFFER_SIZE);
 
-  if (info.dwMajorVersion == 4)
-      switch (info.dwMinorVersion) {
-      case 90 :
-          fprintf(unit,"# System                : Windows Me\n");
-          break;
-      case 10 :
-          fprintf(unit,"# System                : Windows 98\n");
-          break;
-      case 0 :
-          fprintf(unit,"# System                : Windows NT 4.0\n");
-          break;
-      default :
-          break;
-      }
-  else if (info.dwMajorVersion == 5)
-      switch (info.dwMinorVersion) {
-      case 2 :
-          fprintf(unit,"# System                : Windows 2003\n");
-          break;
-      case 1 :
-          fprintf(unit,"# System                : Windows XP\n");
-          break;
-      case 0 :
-          fprintf(unit,"# System                : Windows 2000\n");
-          break;
-      default :
-          break;
-      }
-  else if (info.dwMajorVersion == 6)
-      switch (info.dwMinorVersion) {
-      case 0 :
-          if (info.wProductType == VER_NT_WORKSTATION)
-              fprintf(unit,"# System                : Windows Vista\n");
-          else
-              fprintf(unit,"# System                : Windows Server 2008\n");
-          break;
-      default :
-          break;
-      }
-  
-  fprintf(unit,"# Release               : %-d.%-d.%-d\n",info.dwMajorVersion,
-          info.dwMinorVersion,info.dwBuildNumber);
-  fprintf(unit,"# Version               : %s\n",info.szCSDVersion);
+    /* Replace  "Intel64" by "Intel(R) 64" */
+    substr_ptr = strstr(infoBuf, "Intel64");
+    if (substr_ptr != NULL)
+        fprintf(unit, "# Machine               : Intel(R) 64%s\n", substr_ptr + strlen("Intel64"));
+    else {
+        /* Replace  "EM64T" by "Intel(R) 64" */
+        substr_ptr = strstr(infoBuf, "EM64T");
+        if (substr_ptr != NULL)
+            fprintf(unit, "# Machine               : Intel(R) 64%s\n", substr_ptr + strlen("EM64T"));
+        else
+            fprintf(unit, "# Machine               : %s\n", infoBuf);
+    }
+
+    if (info.dwMajorVersion == 4)
+        switch (info.dwMinorVersion) {
+            case 90:
+                fprintf(unit, "# System                : Windows Me\n");
+                break;
+            case 10:
+                fprintf(unit, "# System                : Windows 98\n");
+                break;
+            case 0:
+                fprintf(unit, "# System                : Windows NT 4.0\n");
+                break;
+            default:
+                break;
+    }
+    else if (info.dwMajorVersion == 5)
+        switch (info.dwMinorVersion) {
+            case 2:
+                fprintf(unit, "# System                : Windows 2003\n");
+                break;
+            case 1:
+                fprintf(unit, "# System                : Windows XP\n");
+                break;
+            case 0:
+                fprintf(unit, "# System                : Windows 2000\n");
+                break;
+            default:
+                break;
+    }
+    else if (info.dwMajorVersion == 6)
+        switch (info.dwMinorVersion) {
+            case 0:
+                if (info.wProductType == VER_NT_WORKSTATION)
+                    fprintf(unit, "# System                : Windows Vista\n");
+                else
+                    fprintf(unit, "# System                : Windows Server 2008\n");
+                break;
+            default:
+                break;
+    }
+
+    fprintf(unit, "# Release               : %-d.%-d.%-d\n", info.dwMajorVersion,
+            info.dwMinorVersion, info.dwBuildNumber);
+    fprintf(unit, "# Version               : %s\n", info.szCSDVersion);
 #endif
-/* >> IMB 3.1  */
-  fprintf(unit,"# MPI Version           : %-d.%-d\n",mpi_version,mpi_subversion);
-  fprintf(unit,"# MPI Thread Environment: ");
+    /* >> IMB 3.1  */
+    fprintf(unit, "# MPI Version           : %-d.%-d\n", mpi_version, mpi_subversion);
+    fprintf(unit, "# MPI Thread Environment: ");
 
 #ifdef USE_MPI_INIT_THREAD
-  switch (mpi_thread_environment) 
-  {
-    case MPI_THREAD_SINGLE :
-	fprintf(unit,"MPI_THREAD_SINGLE\n");
-	break;
+    switch (mpi_thread_environment)
+    {
+        case MPI_THREAD_SINGLE:
+            fprintf(unit, "MPI_THREAD_SINGLE\n");
+            break;
 
-    case MPI_THREAD_FUNNELED :
-	fprintf(unit,"MPI_THREAD_FUNNELED\n");
-	break;
+        case MPI_THREAD_FUNNELED:
+            fprintf(unit, "MPI_THREAD_FUNNELED\n");
+            break;
 
-    case MPI_THREAD_SERIALIZED :
-	fprintf(unit,"MPI_THREAD_SERIALIZED\n");
-	break;
+        case MPI_THREAD_SERIALIZED:
+            fprintf(unit, "MPI_THREAD_SERIALIZED\n");
+            break;
 
-    default :
-	fprintf(unit,"MPI_THREAD_MULTIPLE\n");
-	break;
-  }
+        default:
+            fprintf(unit, "MPI_THREAD_MULTIPLE\n");
+            break;
+    }
 #endif
 
-// IMB 3.2 add on: Version information to stdout
-  if( strcmp(VERSION,"3.2") >0 ) {
-    fprintf(unit,"\n\n# New default behavior from Version 3.2 on:\n\n");
-    fprintf(unit,"\
-# the number of iterations per message size is cut down \n\
-# dynamically when a certain run time (per message size sample) \n\
-# is expected to be exceeded. Time limit is defined by variable \n\
-# \"SECS_PER_SAMPLE\" (=> IMB_settings.h) \n\
-# or through the flag => -time \n\
-  ");
-  }
+    // IMB 3.2 add on: Version information to stdout
+    if (strcmp(VERSION, "3.2") > 0) {
+        fprintf(unit, "\n\n# New default behavior from Version 3.2 on:\n\n");
+        fprintf(unit, "\
+                      # the number of iterations per message size is cut down \n\
+                      # dynamically when a certain run time (per message size sample) \n\
+                      # is expected to be exceeded. Time limit is defined by variable \n\
+                      # \"SECS_PER_SAMPLE\" (=> IMB_settings.h) \n\
+                      # or through the flag => -time \n\
+                        ");
+    }
 }
 
-void IMB_end_msg(struct comm_info* c_info )
+void IMB_end_msg(struct comm_info* c_info) {
 /*
 
-                      
                       Prints to stdout an eventual end message (currently empty)
-                      
 
+Input variables:
 
-Input variables: 
-
--c_info               (type struct comm_info*)                      
+-c_info               (type struct comm_info*)
                       Collection of all base data for MPI;
                       see [1] for more information
-                      
-
 
 */
-{
+    FILE* u;
 
-FILE* u;
-
-if ( c_info ) {
-
-if( c_info->w_rank == 0 ) {
-
-for( u=stdout; u; u=(u==unit ? NULL : unit) )
-{
-fprintf(u,"\n\n# All processes entering MPI_Finalize\n\n");
-}
-
-}
-}
-else
-{
-fprintf(stderr,"\n\n# IMB has MPI_Finalize-d\n\n");
-}
-
+    if (c_info) {
+        if (c_info->w_rank == 0) {
+            for (u = stdout; u; u = (u == unit ? NULL : unit))
+                fprintf(u, "\n\n# All processes entering MPI_Finalize\n\n");
+        }
+    } else
+        fprintf(stderr, "\n\n# IMB has MPI_Finalize-d\n\n");
 }
