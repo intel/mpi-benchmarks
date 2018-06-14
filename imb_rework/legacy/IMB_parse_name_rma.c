d3399e78378a20566e21a104a05e17daec8c0064
vladimir.vinnitski <vvinnits@nnlmpihsw02.inn.intel.com>
2018-06-14 10:54:04 +0300

    Code cleanup(whitespace). IMB_parse_name_rma.c

--- imb_rework/legacy/IMB_parse_name_rma.c
+++ imb_rework/legacy/IMB_parse_name_rma.c
@@ -80,211 +80,161 @@ For more documentation than found here, see
 #include "IMB_prototypes.h"
 
 
-int IMB_get_def_cases(char*** defc, char*** Gcmt)
-{
+int IMB_get_def_cases(char*** defc, char*** Gcmt) {
     *defc = &DEFC[0];
     *Gcmt = &Gral_cmt[0];
-    return (int) (sizeof(DEFC)/sizeof(char*));
+    return (int)(sizeof(DEFC) / sizeof(char*));
 }
 
-int IMB_get_all_cases(char*** allc)
+int IMB_get_all_cases(char*** allc) {
 /*
 
-                      
                       Initializes default benchmark names (defc) and accompanying
                       comments (Gcmt)
-                      
 
+In/out variables:
 
-In/out variables: 
-
--defc                 (type char***)                      
+-defc                 (type char***)
                       List of benchkark names (strings)
-                      
 
--Gcmt                 (type char***)                      
+-Gcmt                 (type char***)
                       List of general comments (strings)
-                      
-
 
 */
-{
     *allc = &ALLC[0];
-    return (int) (sizeof(ALLC)/sizeof(char*));
+    return (int)(sizeof(ALLC) / sizeof(char*));
 }
 
 
-void IMB_set_bmark(struct Bench* bmark)
-{
+void IMB_set_bmark(struct Bench* bmark) {
     BTYPES type;
-    
+
     bmark->bench_comments = NIL_COMMENT;
     bmark->N_Modes = 2;
     bmark->Ntimes = 1;
     bmark->reduction = 0;
-    
-    bmark->RUN_MODES[0].AGGREGATE=0;
-    bmark->RUN_MODES[1].AGGREGATE=1;
-    bmark->RUN_MODES[0].NONBLOCKING =0;
-    bmark->RUN_MODES[1].NONBLOCKING =0;
-    bmark->RUN_MODES[0].BIDIR =0;
-    bmark->RUN_MODES[1].BIDIR =0;
-    
-    
-    if (!strcmp(bmark->name,"unidir_put"))
-    { 
+
+    bmark->RUN_MODES[0].AGGREGATE = 0;
+    bmark->RUN_MODES[1].AGGREGATE = 1;
+    bmark->RUN_MODES[0].NONBLOCKING = 0;
+    bmark->RUN_MODES[1].NONBLOCKING = 0;
+    bmark->RUN_MODES[0].BIDIR = 0;
+    bmark->RUN_MODES[1].BIDIR = 0;
+
+
+    if (!strcmp(bmark->name, "unidir_put")) {
         bmark->Benchmark = IMB_rma_single_put;
         type = SingleTransfer;
         bmark->access = put;
-    }
-    else if (!strcmp(bmark->name,"unidir_get"))
-    { 
+    } else if (!strcmp(bmark->name, "unidir_get")) {
         bmark->Benchmark = IMB_rma_single_get;
         type = SingleTransfer;
         bmark->access = get;
-    }
-    else if (!strcmp(bmark->name,"bidir_put"))
-    { 
+    } else if (!strcmp(bmark->name, "bidir_put")) {
         bmark->Benchmark = IMB_rma_single_put;
         type = SingleTransfer;
         bmark->access = put;
         bmark->RUN_MODES[0].BIDIR = 1;
         bmark->RUN_MODES[1].BIDIR = 1;
 
-    }
-    else if (!strcmp(bmark->name,"bidir_get"))
-    { 
+    } else if (!strcmp(bmark->name, "bidir_get")) {
         bmark->Benchmark = IMB_rma_single_get;
         type = SingleTransfer;
         bmark->access = get;
         bmark->RUN_MODES[0].BIDIR = 1;
         bmark->RUN_MODES[1].BIDIR = 1;
 
-    }
-    else if (!strcmp(bmark->name,"put_local"))
-    { 
+    } else if (!strcmp(bmark->name, "put_local")) {
         bmark->Benchmark = IMB_rma_put_local;
         type = SingleTransfer;
         bmark->access = put;
-    }
-    else if (!strcmp(bmark->name,"get_local"))
-    { 
+    } else if (!strcmp(bmark->name, "get_local")) {
         bmark->Benchmark = IMB_rma_get_local;
         type = SingleTransfer;
         bmark->access = get;
-    }
-    else if (!strcmp(bmark->name,"put_all_local"))
-    { 
+    } else if (!strcmp(bmark->name, "put_all_local")) {
         bmark->N_Modes = 1;
-        bmark->RUN_MODES[0].AGGREGATE=-1;
+        bmark->RUN_MODES[0].AGGREGATE = -1;
         bmark->Benchmark = IMB_rma_put_all_local;
         type = MultPassiveTransfer;
         bmark->access = put;
-    }
-    else if (!strcmp(bmark->name,"get_all_local"))
-    { 
+    } else if (!strcmp(bmark->name, "get_all_local")) {
         bmark->N_Modes = 1;
-        bmark->RUN_MODES[0].AGGREGATE=-1;
+        bmark->RUN_MODES[0].AGGREGATE = -1;
         bmark->Benchmark = IMB_rma_get_all_local;
         type = MultPassiveTransfer;
         bmark->access = get;
-    }
-    else if (!strcmp(bmark->name,"one_put_all"))
-    { 
+    } else if (!strcmp(bmark->name, "one_put_all")) {
         bmark->N_Modes = 1;
         bmark->Benchmark = IMB_rma_put_all;
         type = MultPassiveTransfer;
         bmark->access = put;
-        bmark->RUN_MODES[0].AGGREGATE=-1;
-
-    }
-    else if (!strcmp(bmark->name,"one_get_all"))
-    { 
+        bmark->RUN_MODES[0].AGGREGATE = -1;
+    } else if (!strcmp(bmark->name, "one_get_all")) {
         bmark->N_Modes = 1;
         bmark->Benchmark = IMB_rma_get_all;
         type = MultPassiveTransfer;
         bmark->access = get;
-        bmark->RUN_MODES[0].AGGREGATE=-1;
-
-    }
-    else if (!strcmp(bmark->name,"all_put_all"))
-    { 
+        bmark->RUN_MODES[0].AGGREGATE = -1;
+    } else if (!strcmp(bmark->name, "all_put_all")) {
         bmark->N_Modes = 1;
         bmark->Benchmark = IMB_rma_put_all;
         type = Collective;
         bmark->access = put;
-        bmark->RUN_MODES[0].AGGREGATE=-1;
-    }
-    else if (!strcmp(bmark->name,"all_get_all"))
-    { 
+        bmark->RUN_MODES[0].AGGREGATE = -1;
+    } else if (!strcmp(bmark->name, "all_get_all")) {
         bmark->N_Modes = 1;
         bmark->Benchmark = IMB_rma_get_all;
         type = Collective;
         bmark->access = get;
-        bmark->RUN_MODES[0].AGGREGATE=-1;
-    }
-    else if (!strcmp(bmark->name,"exchange_put"))
-    { 
+        bmark->RUN_MODES[0].AGGREGATE = -1;
+    } else if (!strcmp(bmark->name, "exchange_put")) {
         bmark->N_Modes = 1;
         bmark->Benchmark = IMB_rma_exchange_put;
         type = Collective;
         bmark->access = put;
-        bmark->RUN_MODES[0].AGGREGATE=-1;
-    }
-    else if (!strcmp(bmark->name,"exchange_get"))
-    { 
+        bmark->RUN_MODES[0].AGGREGATE = -1;
+    } else if (!strcmp(bmark->name, "exchange_get")) {
         bmark->N_Modes = 1;
         bmark->Benchmark = IMB_rma_exchange_get;
         type = Collective;
         bmark->access = get;
-        bmark->RUN_MODES[0].AGGREGATE=-1;
-    }
-    else if (!strcmp(bmark->name,"accumulate"))
-    { 
+        bmark->RUN_MODES[0].AGGREGATE = -1;
+    } else if (!strcmp(bmark->name, "accumulate")) {
         bmark->Benchmark = IMB_rma_accumulate;
         type = SingleTransfer;
         bmark->access = put;
         bmark->reduction = 1;
-    }
-    else if (!strcmp(bmark->name,"get_accumulate"))
-    { 
+    } else if (!strcmp(bmark->name, "get_accumulate")) {
         bmark->Benchmark = IMB_rma_get_accumulate;
         type = SingleTransfer;
         bmark->access = put;
         bmark->reduction = 1;
-    }
-    else if (!strcmp(bmark->name,"fetch_and_op"))
-    { 
+    } else if (!strcmp(bmark->name, "fetch_and_op")) {
         bmark->Benchmark = IMB_rma_fetch_and_op;
         type = SingleElementTransfer;
         bmark->access = put;
         bmark->reduction = 1;
-    }
-    else if (!strcmp(bmark->name,"compare_and_swap"))
-    { 
+    } else if (!strcmp(bmark->name, "compare_and_swap")) {
         bmark->Benchmark = IMB_rma_compare_and_swap;
         type = SingleElementTransfer;
         bmark->access = put;
         bmark->bench_comments = Compare_and_swap_cmt;
-    }
-    else if (!strcmp(bmark->name,"truly_passive_put"))
-    { 
+    } else if (!strcmp(bmark->name, "truly_passive_put")) {
         bmark->Benchmark = IMB_rma_passive_put;
         type = SingleTransfer;
         bmark->access = put;
         bmark->N_Modes = 1;
         bmark->Ntimes = 2;
-        bmark->RUN_MODES[0].AGGREGATE=-1;
-        bmark->RUN_MODES[0].NONBLOCKING=1;
+        bmark->RUN_MODES[0].AGGREGATE = -1;
+        bmark->RUN_MODES[0].NONBLOCKING = 1;
         bmark->bench_comments = Truly_passive_put_cmt;
-    }
-    else 
-    {
+    } else {
         bmark->RUN_MODES[0].type = BTYPE_INVALID;
         bmark->RUN_MODES[1].type = BTYPE_INVALID;
         return;
     }
-    
+
     bmark->RUN_MODES[0].type = type;
     bmark->RUN_MODES[1].type = type;
     bmark->name[0] = toupper(bmark->name[0]);
