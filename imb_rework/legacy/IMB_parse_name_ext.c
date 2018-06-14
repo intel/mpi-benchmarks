c24e19fab93cc361a1945dc87a42508496b03c0f
vladimir.vinnitski <vvinnits@nnlmpihsw02.inn.intel.com>
2018-06-14 10:53:06 +0300

    Code cleanup(whitespace). IMB_parse_name_ext.c

--- imb_rework/legacy/IMB_parse_name_ext.c
+++ imb_rework/legacy/IMB_parse_name_ext.c
@@ -80,98 +80,78 @@ For more documentation than found here, see
 #include "IMB_prototypes.h"
 
 
-int IMB_get_def_cases(char*** defc, char*** Gcmt)
+int IMB_get_def_cases(char*** defc, char*** Gcmt) {
 /*
 
-                      
-                      Initializes default benchmark names (defc) and accompanying
-                      comments (Gcmt)
-                      
+                          Initializes default benchmark names (defc) and accompanying
+                          comments (Gcmt)
 
+In/out variables:
 
-In/out variables: 
+-defc                     (type char***)
+                          List of benchkark names (strings)
 
--defc                 (type char***)                      
-                      List of benchkark names (strings)
-                      
-
--Gcmt                 (type char***)                      
-                      List of general comments (strings)
-                      
-
+-Gcmt                     (type char***)
+                          List of general comments (strings)
 
 */
-{
     *defc = &DEFC[0];
     *Gcmt = &Gral_cmt[0];
-    return (int) (sizeof(DEFC)/sizeof(char*));
+    return (int)(sizeof(DEFC) / sizeof(char*));
 }
 
-int IMB_get_all_cases(char*** allc)
+int IMB_get_all_cases(char*** allc) {
 /*
 
-                      
                       Initializes default benchmark names (defc) and accompanying
                       comments (Gcmt)
-                      
 
+In/out variables:
 
-In/out variables: 
-
--defc                 (type char***)                      
+-defc                 (type char***)
                       List of benchkark names (strings)
-                      
 
--Gcmt                 (type char***)                      
+-Gcmt                 (type char***)
                       List of general comments (strings)
-                      
-
 
 */
-{
     *allc = &ALLC[0];
-    return (int) (sizeof(ALLC)/sizeof(char*));
+    return (int)(sizeof(ALLC) / sizeof(char*));
 }
 
 
-void IMB_set_bmark(struct Bench* Bmark)
+void IMB_set_bmark(struct Bench* Bmark) {
 /*
 
+In/out variables:
 
-
-In/out variables: 
-
--Bmark                (type struct Bench*)                      
+-Bmark                (type struct Bench*)
                       (For explanation of struct Bench type:
                       describes all aspects of modes of a benchmark;
                       see [1] for more information)
-                      
+
                       On input, only the name of the benchmark is set.
                       On output, all necessary run modes are set accordingly
-                      
-
 
 */
-{
     BTYPES type;
 #if 0
     int index;
 
-    IMB_get_def_index(&index,Bmark->name);
+    IMB_get_def_index(&index, Bmark->name);
 
-    if( index < 0 )
-    {
-	Bmark->RUN_MODES[0].type=BTYPE_INVALID;
-	Bmark->RUN_MODES[1].type=BTYPE_INVALID;
-	return;
+    if (index < 0) {
+        Bmark->RUN_MODES[0].type = BTYPE_INVALID;
+        Bmark->RUN_MODES[1].type = BTYPE_INVALID;
+        return;
     }
 #endif
 
-    Bmark->RUN_MODES[0].AGGREGATE=1;
-    Bmark->RUN_MODES[1].AGGREGATE=0;
+    Bmark->RUN_MODES[0].AGGREGATE = 1;
+    Bmark->RUN_MODES[1].AGGREGATE = 0;
 
-    Bmark->RUN_MODES[0].NONBLOCKING =0;
-    Bmark->RUN_MODES[1].NONBLOCKING =0;
+    Bmark->RUN_MODES[0].NONBLOCKING = 0;
+    Bmark->RUN_MODES[1].NONBLOCKING = 0;
 
     Bmark->N_Modes = 2;
 
@@ -179,63 +159,50 @@ In/out variables:
 
     Bmark->Ntimes = 1;
 
-    if (!strcmp(Bmark->name,"unidir_get"))
-    { 
-	strcpy(Bmark->name,"Unidir_Get");
-	Bmark->Benchmark = IMB_unidir_get;
-	Bmark->bench_comments = &Unidir_Get_cmt[0];
-	type = SingleTransfer;
-	Bmark->access = get;
+    if (!strcmp(Bmark->name, "unidir_get")) {
+        strcpy(Bmark->name, "Unidir_Get");
+        Bmark->Benchmark = IMB_unidir_get;
+        Bmark->bench_comments = &Unidir_Get_cmt[0];
+        type = SingleTransfer;
+        Bmark->access = get;
+    } else if (!strcmp(Bmark->name, "unidir_put")) {
+        strcpy(Bmark->name, "Unidir_Put");
+        Bmark->Benchmark = IMB_unidir_put;
+        Bmark->bench_comments = &Unidir_Put_cmt[0];
+        type = SingleTransfer;
+        Bmark->access = put;
+    } else if (!strcmp(Bmark->name, "bidir_get")) {
+        strcpy(Bmark->name, "Bidir_Get");
+        Bmark->Benchmark = IMB_bidir_get;
+        Bmark->bench_comments = &Bidir_Get_cmt[0];
+        type = SingleTransfer;
+        Bmark->access = get;
+    } else if (!strcmp(Bmark->name, "bidir_put")) {
+        strcpy(Bmark->name, "Bidir_Put");
+        Bmark->Benchmark = IMB_bidir_put;
+        Bmark->bench_comments = &Bidir_Put_cmt[0];
+        type = SingleTransfer;
+        Bmark->access = put;
+    } else if (!strcmp(Bmark->name, "accumulate")) {
+        strcpy(Bmark->name, "Accumulate");
+        Bmark->Benchmark = IMB_accumulate;
+        Bmark->bench_comments = &Accumulate_cmt[0];
+        type = Collective;
+        Bmark->access = put;
+        Bmark->reduction = 1;
+    } else if (!strcmp(Bmark->name, "window")) {
+        strcpy(Bmark->name, "Window");
+        Bmark->Benchmark = IMB_window;
+        Bmark->bench_comments = &Window_cmt[0];
+        Bmark->RUN_MODES[0].AGGREGATE = -1;
+        Bmark->N_Modes = 1;
+        type = Collective;
+        Bmark->access = no;
+        Bmark->reduction = 1;
+    } else {
+        type = BTYPE_INVALID;
     }
-    else if (!strcmp(Bmark->name,"unidir_put"))
-    { 
-	strcpy(Bmark->name,"Unidir_Put");
-	Bmark->Benchmark = IMB_unidir_put;
-	Bmark->bench_comments = &Unidir_Put_cmt[0];
-	type = SingleTransfer;
-	Bmark->access = put;
-    }
-    else if (!strcmp(Bmark->name,"bidir_get"))
-    { 
-	strcpy(Bmark->name,"Bidir_Get");
-	Bmark->Benchmark = IMB_bidir_get;
-	Bmark->bench_comments = &Bidir_Get_cmt[0];
-	type = SingleTransfer;
-	Bmark->access = get;
-    }
-    else if (!strcmp(Bmark->name,"bidir_put"))
-    { 
-	strcpy(Bmark->name,"Bidir_Put");
-	Bmark->Benchmark = IMB_bidir_put;
-	Bmark->bench_comments = &Bidir_Put_cmt[0];
-	type = SingleTransfer;
-	Bmark->access = put;
-    }
-    else if (!strcmp(Bmark->name,"accumulate"))
-    { 
-	strcpy(Bmark->name,"Accumulate");
-	Bmark->Benchmark = IMB_accumulate;
-	Bmark->bench_comments = &Accumulate_cmt[0];
-	type = Collective;
-	Bmark->access = put;
-	Bmark->reduction = 1;
-    }
-    else if (!strcmp(Bmark->name,"window"))
-    { 
-	strcpy(Bmark->name,"Window");
-	Bmark->Benchmark = IMB_window;
-	Bmark->bench_comments = &Window_cmt[0];
-	Bmark->RUN_MODES[0].AGGREGATE=-1;
-	Bmark->N_Modes = 1;
-	type = Collective;
-	Bmark->access = no;
-	Bmark->reduction = 1;
-    }
-    else 
-    {
-	type = BTYPE_INVALID;
-    }
-    Bmark->RUN_MODES[0].type=type;
-    Bmark->RUN_MODES[1].type=type;
+    Bmark->RUN_MODES[0].type = type;
+    Bmark->RUN_MODES[1].type = type;
 
 }
