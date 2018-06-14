e04b0e7c22c41cd928a4dadab06a8975ad9b689b
vladimir.vinnitski <vvinnits@nnlmpihsw02.inn.intel.com>
2018-06-14 10:51:39 +0300

    Code cleanup(whitespace). IMB_init_transfer.c

--- imb_rework/legacy/IMB_init_transfer.c
+++ imb_rework/legacy/IMB_init_transfer.c
@@ -79,134 +79,119 @@ For more documentation than found here, see
 
 
 /* IMB 3.1 << */
-void IMB_init_transfer(struct comm_info* c_info, struct Bench* Bmark, int size, MPI_Aint acc_size)
+void IMB_init_transfer(struct comm_info* c_info, struct Bench* Bmark, int size, MPI_Aint acc_size) {
 /* >> IMB 3.1  */
 /*
 
-                      
-                      For IO  case: file splitting/view is set, file is opened
-                      For EXT case: window is created and synchronized (MPI_Win_fence)
-                      
+                          For IO  case: file splitting/view is set, file is opened
+                          For EXT case: window is created and synchronized (MPI_Win_fence)
 
+Input variables:
 
-Input variables: 
+-Bmark                    (type struct Bench*)
+                          (For explanation of struct Bench type:
+                          describes all aspects of modes of a benchmark;
+                          see [1] for more information)
 
--Bmark                (type struct Bench*)                      
-                      (For explanation of struct Bench type:
-                      describes all aspects of modes of a benchmark;
-                      see [1] for more information)
-                      
-                      Given benchmark
-                      
+                          Given benchmark
 
--size                 (type int)                      
-                      (Only IO case): used to determine file view
-                      
+-size                     (type int)
+                          (Only IO case): used to determine file view
+
 IMB 3.1 <<
--acc_size             (type int)                      
-                      (Only EXT case): accumulate window size
+-acc_size                 (type int)
+                          (Only EXT case): accumulate window size
 >> IMB 3.1
 
+In/out variables:
 
-In/out variables: 
+-c_info                   (type struct comm_info*)
+                          Collection of all base data for MPI;
+                          see [1] for more information
 
--c_info               (type struct comm_info*)                      
-                      Collection of all base data for MPI;
-                      see [1] for more information
-                      
-                      Corresponding components (File or Window related) are set
-                      
-
+                          Corresponding components (File or Window related) are set
 
 */
-{
-
 
 #if defined MPIIO
-    #include <limits.h>	
+#include <limits.h>
     int ne, baslen, mod;
     int ierr;
     size_t pos1, pos2;
 
-    if( c_info->File_rank < 0 || Bmark->access == no ) return;
+    if (c_info->File_rank < 0 || Bmark->access == no) return;
 
-    if( size > 0)
-    {
-	IMB_get_rank_portion(c_info->File_rank, c_info->all_io_procs, size, asize,
-			     &pos1, &pos2);
-	baslen =  (pos2 >= pos1) ? pos2-pos1+1 : 0;
-    } else
-    {
-	baslen = 0;
-	pos2 = pos1 = 0;
+    if (size > 0) {
+        IMB_get_rank_portion(c_info->File_rank, c_info->all_io_procs, size, asize,
+                             &pos1, &pos2);
+        baslen = (pos2 >= pos1) ? pos2 - pos1 + 1 : 0;
+    } else {
+        baslen = 0;
+        pos2 = pos1 = 0;
     }
 
-    if( c_info->view != MPI_DATATYPE_NULL )
-		    MPI_Type_free(&c_info->view);
+    if (c_info->view != MPI_DATATYPE_NULL)
+        MPI_Type_free(&c_info->view);
 
-    if( Bmark->fpointer == priv )
-    {
+    if (Bmark->fpointer == priv) {
+        c_info->split.Locsize = baslen;
+        c_info->split.Offset = 0;
+        c_info->split.Totalsize = baslen;
 
-	c_info->split.Locsize	= baslen;
-	c_info->split.Offset	= 0;
-	c_info->split.Totalsize	= baslen;
+        if (Bmark->access == put)
+            IMB_set_buf(c_info, c_info->File_rank, 0,
+                        (baslen > 0) ? baslen - 1 : 0,
+                        1, 0);
 
-	if( Bmark->access == put )
-		IMB_set_buf(c_info, c_info->File_rank, 0, 
-			    (baslen>0)? baslen-1 : 0 ,
-			     1, 0);
-
-	if( Bmark->access == get )
-		IMB_set_buf(c_info, c_info->File_rank, 1, 0, 0,
-			     (baslen>0)? baslen-1 : 0 );
-  
+        if (Bmark->access == get)
+            IMB_set_buf(c_info, c_info->File_rank, 1, 0, 0,
+                        (baslen > 0) ? baslen - 1 : 0);
     }
 
-    if( Bmark->fpointer == indv_block || Bmark->fpointer == shared ||
-	Bmark->fpointer == explic )
-    {
-	int bllen[3];
+    if (Bmark->fpointer == indv_block || Bmark->fpointer == shared ||
+        Bmark->fpointer == explic) {
+        int bllen[3];
 
-	MPI_Aint displ[3];
-	MPI_Datatype types[3];
+        MPI_Aint displ[3];
+        MPI_Datatype types[3];
 
-	bllen[0]=1; displ[0] = 0; types[0] = MPI_LB;
+        bllen[0] = 1;
+        displ[0] = 0;
+        types[0] = MPI_LB;
 
-	bllen[1] = baslen;
-	displ[1] = pos1;
-	types[1] = c_info->etype;
+        bllen[1] = baslen;
+        displ[1] = pos1;
+        types[1] = c_info->etype;
 
-	bllen[2] = 1;
-	displ[2] = size;
-	types[2] = MPI_UB;
+        bllen[2] = 1;
+        displ[2] = size;
+        types[2] = MPI_UB;
 
-	if( Bmark->fpointer == indv_block )
-	{
-	    /* July 2002 fix V2.2.1: handle empty view case separately */
-	    if( baslen>0 )
-	    {
-		/* end change */
-		ierr=MPI_Type_struct(3,bllen,displ,types,&c_info->view);
-		IMB_err_hand(1,ierr);
-		ierr=MPI_Type_commit(&c_info->view);
-		IMB_err_hand(1,ierr);
-		c_info->filetype = c_info->view;
+        if (Bmark->fpointer == indv_block) {
+            /* July 2002 fix V2.2.1: handle empty view case separately */
+            if (baslen > 0) {
+                /* end change */
+                ierr = MPI_Type_struct(3, bllen, displ, types, &c_info->view);
+                IMB_err_hand(1, ierr);
+                ierr = MPI_Type_commit(&c_info->view);
+                IMB_err_hand(1, ierr);
+                c_info->filetype = c_info->view;
 
-		/* July 2002 fix V2.2.1: handle empty case */
-	    }
-	    else c_info->filetype = c_info->etype;
-	    /* end change */
-	}
+                /* July 2002 fix V2.2.1: handle empty case */
+            } else
+                c_info->filetype = c_info->etype;
+            /* end change */
+        }
 
-	if( Bmark->access == put )
-	    IMB_set_buf(c_info, c_info->File_rank, 0, (baslen>0)? baslen-1 : 0, 1, 0 );
+        if (Bmark->access == put)
+            IMB_set_buf(c_info, c_info->File_rank, 0, (baslen > 0) ? baslen - 1 : 0, 1, 0);
 
-	if( Bmark->access == get )
-	    IMB_set_buf(c_info, c_info->File_rank, 1, 0, 0, (baslen>0)? baslen-1 : 0 );
+        if (Bmark->access == get)
+            IMB_set_buf(c_info, c_info->File_rank, 1, 0, 0, (baslen > 0) ? baslen - 1 : 0);
 
-	c_info->split.Locsize = bllen[1];
-	c_info->split.Offset  = pos1;
-	c_info->split.Totalsize = size;
+        c_info->split.Locsize = bllen[1];
+        c_info->split.Offset = pos1;
+        c_info->split.Totalsize = size;
     }
 
     ierr = IMB_open_file(c_info);
@@ -216,73 +201,59 @@ In/out variables:
     int s_size, r_size;
     int ierr;
 
-    ierr=0;
+    ierr = 0;
 
-    if( Bmark->reduction )
-    {
-	MPI_Type_size(c_info->red_data_type,&s_size);
-	r_size=s_size;
-    }
-    else
-    {
-	MPI_Type_size(c_info->s_data_type,&s_size);
-	MPI_Type_size(c_info->r_data_type,&r_size);
+    if (Bmark->reduction) {
+        MPI_Type_size(c_info->red_data_type, &s_size);
+        r_size = s_size;
+    } else {
+        MPI_Type_size(c_info->s_data_type, &s_size);
+        MPI_Type_size(c_info->r_data_type, &r_size);
     }
 
-    if( c_info -> rank >= 0 )
-    {
-	IMB_user_set_info(&c_info->info);
+    if (c_info->rank >= 0) {
+        IMB_user_set_info(&c_info->info);
 
-	/* IMB 3.1 << */
-	sz = acc_size;
-	/* >> IMB 3.1  */
+        /* IMB 3.1 << */
+        sz = acc_size;
+        /* >> IMB 3.1  */
 
-	if( Bmark->access == put)
-	{
-	    ierr = MPI_Win_create(c_info->r_buffer,sz,r_size,c_info->info,
-				  c_info->communicator, &c_info->WIN);
-	    MPI_ERRHAND(ierr);
-	    ierr = MPI_Win_fence(0, c_info->WIN);
-	    MPI_ERRHAND(ierr);
-	}
-	else if( Bmark->access == get)
-	{
-	    ierr = MPI_Win_create(c_info->s_buffer,sz,s_size,c_info->info,
-				  c_info->communicator, &c_info->WIN);
-	    MPI_ERRHAND(ierr);
-	    ierr = MPI_Win_fence(0, c_info->WIN);
-	    MPI_ERRHAND(ierr);
-	}
+        if (Bmark->access == put) {
+            ierr = MPI_Win_create(c_info->r_buffer, sz, r_size, c_info->info,
+                                  c_info->communicator, &c_info->WIN);
+            MPI_ERRHAND(ierr);
+            ierr = MPI_Win_fence(0, c_info->WIN);
+            MPI_ERRHAND(ierr);
+        } else if (Bmark->access == get) {
+            ierr = MPI_Win_create(c_info->s_buffer, sz, s_size, c_info->info,
+                                  c_info->communicator, &c_info->WIN);
+            MPI_ERRHAND(ierr);
+            ierr = MPI_Win_fence(0, c_info->WIN);
+            MPI_ERRHAND(ierr);
+        }
     }
 #elif defined RMA
     int s_size, r_size;
     int ierr = 0;
 
-    if(Bmark->reduction)
-    {
-        MPI_Type_size(c_info->red_data_type,&s_size);
-        r_size=s_size;
-    }
-    else
-    {
-        MPI_Type_size(c_info->s_data_type,&s_size);
-        MPI_Type_size(c_info->r_data_type,&r_size);
+    if (Bmark->reduction) {
+        MPI_Type_size(c_info->red_data_type, &s_size);
+        r_size = s_size;
+    } else {
+        MPI_Type_size(c_info->s_data_type, &s_size);
+        MPI_Type_size(c_info->r_data_type, &r_size);
     }
 
-    if(c_info->rank >= 0)
-    {
+    if (c_info->rank >= 0) {
         IMB_user_set_info(&c_info->info);
 
-        if (Bmark->access == put)
-        {    
+        if (Bmark->access == put) {
             ierr = MPI_Win_create(c_info->r_buffer, acc_size, r_size, c_info->info,
-                    c_info->communicator, &c_info->WIN);
-        }
-        else if (Bmark->access == get)
-        {
+                                  c_info->communicator, &c_info->WIN);
+        } else if (Bmark->access == get) {
             ierr = MPI_Win_create(c_info->s_buffer, acc_size, r_size, c_info->info,
-                    c_info->communicator, &c_info->WIN);
-        }    
+                                  c_info->communicator, &c_info->WIN);
+        }
         MPI_ERRHAND(ierr);
     }
 #endif 
@@ -292,52 +263,43 @@ In/out variables:
 }
 
 
-void IMB_close_transfer (struct comm_info* c_info, struct Bench* Bmark, int size)
-    /*
-
+void IMB_close_transfer (struct comm_info* c_info, struct Bench* Bmark, int size) {
+/*
 
        Closes / frees file / window components
 
+Input variables:
 
+-Bmark                (type struct Bench*)
+                      (For explanation of struct Bench type:
+                      describes all aspects of modes of a benchmark;
+                      see [1] for more information)
 
-       Input variables: 
+Given benchmark
 
-       -Bmark                (type struct Bench*)                      
-       (For explanation of struct Bench type:
-       describes all aspects of modes of a benchmark;
-       see [1] for more information)
+-size                 (type int)
+                      (Only IO case): used to determine file view
 
-       Given benchmark
+In/out variables:
 
+-c_info               (type struct comm_info*)
+                      Collection of all base data for MPI;
+                      see [1] for more information
 
-       -size                 (type int)                      
-       (Only IO case): used to determine file view
-
-
-
-       In/out variables: 
-
-       -c_info               (type struct comm_info*)                      
-       Collection of all base data for MPI;
-       see [1] for more information
-
-       Corresponding components (File or Window related) are freed
-
-
+                      Corresponding components (File or Window related) are freed
 
 */
-{
 #ifdef MPIIO
-    if( c_info->view != MPI_DATATYPE_NULL )
+    if (c_info->view != MPI_DATATYPE_NULL)
         MPI_Type_free(&c_info->view);
 
-    if( c_info->File_rank >= 0 && Bmark->access != no && c_info->fh!=MPI_FILE_NULL)
+    if (c_info->File_rank >= 0 && Bmark->access != no && c_info->fh != MPI_FILE_NULL)
         MPI_File_close(&c_info->fh);
 
 #else /*not MPIIO*/
 #if (defined EXT || defined RMA)
 
-    if( c_info->WIN != MPI_WIN_NULL )
+    if (c_info->WIN != MPI_WIN_NULL)
         MPI_Win_free(&c_info->WIN);
 
 #endif /*EXT || RMA*/
