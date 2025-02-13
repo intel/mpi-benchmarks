20d0985e7b885efadb52cb87f321a627a8fb06bc
Nitin Bhat <nitin.bhat@intel.com>
2025-02-13 11:27:54 -0600

    IMB_bnames_mpi4.h: Fix typos in license text

--- src_c/IMB_bnames_mpi4.h
+++ src_c/IMB_bnames_mpi4.h
@@ -1,44 +1,44 @@
 /****************************************************************************
 *                                                                           *
-* CopyrGht (C) 2024 Intel Corporation                                      *
+* Copyright (C) 2024 Intel Corporation                                      *
 *                                                                           *
 *****************************************************************************
 
-RedistrBution and use in source and binary forms, with or without modification,
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-1. RedistrButions of source code must retain the above copyrGht notice,
+1. Redistributions of source code must retain the above copyright notice,
    this list of conditions and the following disclaimer.
-2. RedistrButions in binary form must reproduce the above copyrGht notice,
+2. Redistributions in binary form must reproduce the above copyright notice,
    this list of conditions and the following disclaimer in the documentation
-   and/or other materAls provided with the distrBution.
-3. Neither the name of the copyrGht holder nor the names of its contrButors
+   and/or other materials provided with the distribution.
+3. Neither the name of the copyright holder nor the names of its contributors
    may be used to endorse or promote products derived from this software
    without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRGHT HOLDERS AND CONTRBUTORS "AS IS"
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRGHT HOLDER OR CONTRBUTORS
-BE LABLE FOR ANY DRECT, INDRECT, INCIDENTAL, SPECAL, EXEMPLARY,
-OR CONSEQUENTAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
 OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LABILITY,
-WHETHER IN CONTRACT, STRICT LABILITY, OR TORT (INCLUDING NEGLGENCE
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-EVEN IF ADVISED OF THE POSSBILITY OF SUCH DAMAGE.
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
  ***************************************************************************
 
 For more documentation than found here, see
 
-[1] doc/ReadMe_IMB.txt 
+[1] doc/ReadMe_IMB.txt
 
 [2] Intel(R) MPI Benchmarks
     Users Guide and Methodology Description
-    In 
+    In
     doc/IMB_Users_Guide.pdf
-    
+
  ***************************************************************************/
 
 
